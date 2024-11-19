beb9b98157489bc05672acca3fc915ca72f5244b
swethakanala <swetha.bim@gmail.com>
2024-11-19 15:13:48 -0500

    jenkinsfile

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "daws78s-remote-state"
+    bucket = "snowm-wordpress-terraform-state"
     key    = "expense-dev-vpc"
     region = "us-east-1"
     dynamodb_table = "daws78s-locking"
